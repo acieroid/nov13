61f7b79382a30595af6d4fb91915e08926c0979f
Quentin Stievenart <quentin.stievenart@gmail.com>
2012-07-20 21:51:08 +0200

    Fixes attacks

--- action.go
+++ action.go
@@ -45,12 +45,12 @@ func (a *AttackAction) Apply(c *Character, units[]*Character, delta int) {
 			top := a.y - c.damageSize/2
 			bottom := a.y + c.damageSize/2
 			left := a.x - c.damageSize/2
-			right := a.x + c.damageSize
+			right := a.x + c.damageSize/2
 			if unit.Alive() && unit.team == 2 &&
-				(unit.Contains(top, left) ||
-				unit.Contains(top, right) ||
-				unit.Contains(bottom, left) ||
-				unit.Contains(bottom, right)) {
+				(unit.Contains(left, top) ||
+				unit.Contains(right, top) ||
+				unit.Contains(left, bottom) ||
+				unit.Contains(right, bottom)) {
 				damage := Min(unit.life, c.damage)
 				unit.life -= damage
 				if !unit.Alive() {
