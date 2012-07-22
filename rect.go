2eb0cc55f312119f548ddace05b65c88854363cc
Quentin Stievenart <quentin.stievenart@gmail.com>
2012-07-22 12:16:08 +0200

    Fixes attacks (again)

--- rect.go
+++ rect.go
@@ -13,7 +13,7 @@ func (r Rect) Contains(c *Character) bool {
 	left := c.x - CHARACTERSIZE/2
 	right := c.x + CHARACTERSIZE/2
 	top := c.y - CHARACTERSIZE/2
-	bottom := c.y - CHARACTERSIZE/2
+	bottom := c.y + CHARACTERSIZE/2
 	return r.ContainsPoint(left, top) ||
 		r.ContainsPoint(left, bottom) ||
 		r.ContainsPoint(right, top) ||
